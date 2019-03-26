6fc416df48baeea9a0ab2c2cb45b50464c806d8f
hhcgit <hhc50331994@gmail.com>
2019-03-26 09:11:56 -0700

    fixed

--- higherOrderFunctionDrills.js
+++ higherOrderFunctionDrills.js
@@ -1,20 +1,24 @@
 function max(numbers) {
     console.log(numbers)
-    let myMax = numbers[0];
-    for (let i = 0; i < numbers.length; i++) {
+    let myMax = numbers[0];  
+    let i=0;
+    while ( i < numbers.length) {
       if (numbers[i] > myMax) {
         myMax = numbers[i];
       }
+      i++;
     }
     return myMax;
   }
   
   function min(numbers) {
     let myMin = numbers[0];
-    for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] < myMin) {
-        myMin = numbers[i];
+    let j=0;
+    while ( j < numbers.length) {
+      if (numbers[j] < myMin) {
+        myMin = numbers[j];
       }
+      j++;
     }
     return myMin;
   }
